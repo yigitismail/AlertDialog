7fa638367704a35383c210ccfe558273d91fd33e
yigitismail <smlygt@icloud.com>
2020-01-30 21:48:33 +0300

    First

---
+++ app/src/main/java/com/yigitismail/alertdialog/MainActivity.java
@@ -0,0 +1,53 @@
+package com.yigitismail.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,"toast Message",Toast.LENGTH_LONG).show();
+    }
+
+    public void save(View view){
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Save");
+
+        alert.setMessage("Are yue sure ?");
+
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Toast.makeText(MainActivity.this,"Saved",Toast.LENGTH_LONG).show();
+                //Save
+
+            }
+        });
+
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"Not Saved",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        alert.show();
+
+
+
+
+    }
+}
